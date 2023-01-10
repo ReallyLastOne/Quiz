c51d15be35a0219289574458a85e725240cc33c0
ReallyLastOne <ReallyLastOne@op.pl>
2023-01-10 10:51:24 +0100

    Example SQL script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into exercise(id, correct_answer, question, wrong_answer1, wrong_answer2, wrong_answer3, wrong_answer4) values (1, '1410', 'W którym roku odbyła się bitwa pod Grunwaldem?', '1999', '1510', '2001', '1325');
