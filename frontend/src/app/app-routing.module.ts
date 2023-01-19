8f8628a5ed681566b9e574ce653a97518366efd6
d.janoszka <janoszka.damian@wp.pl>
2023-01-19 20:08:07 +0100

    initial_page - adding first routing to another page after button click

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { HomeComponent } from './home/home/home.component';
+import { ExerciseComponent } from './exercise/exercise.component';
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent,
+  },
+  {
+    path: 'exercise', component: ExerciseComponent,
   },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' },
