50e419d0612ef80caa30088b27b5425a71e3b119
ReallyLastOne <reallylastone@op.pl>
2023-01-26 20:21:26 +0100

    document existing endpoints

--- backend/src/java/com/reallylastone/quiz/exercise/question/controller/QuestionExerciseController.java
+++ backend/src/java/com/reallylastone/quiz/exercise/question/controller/QuestionExerciseController.java
@@ -2,6 +2,12 @@ package com.reallylastone.quiz.exercise.question.controller;
 
 import com.reallylastone.quiz.exercise.question.model.QuestionExerciseResponse;
 import com.reallylastone.quiz.exercise.question.service.QuestionExerciseViewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +17,56 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/exercises/questions")
 @RequiredArgsConstructor
+@Tag(
+        description = "Provides operations to work on Question Exercise objects",
+        name = "Question Exercise Controller")
 public class QuestionExerciseController {
     private final QuestionExerciseViewService questionExerciseViewService;
 
+    @Operation(
+            summary = "Gets Question object by its id.", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The specified Question is present.",
+                    content = @Content(schema = @Schema(implementation = QuestionExerciseResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "The specified Question was not found.",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<QuestionExerciseResponse> findById(@PathVariable Long id) {
         return questionExerciseViewService.findById(id);
     }
 
+    @Operation(
+            summary = "Gets all Question object. Supports pagination.", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = QuestionExerciseResponse.class)))
+                    })
+    })
     @GetMapping
     public ResponseEntity<List<QuestionExerciseResponse>> findAll(@RequestParam(defaultValue = "0") Integer page,
                                                                   @RequestParam(defaultValue = "100") Integer pageSize) {
         return questionExerciseViewService.findAll(page, pageSize);
     }
 
+    @Operation(
+            summary = "Gets random Question.", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Random Question is present.",
+                    content = @Content(schema = @Schema(implementation = QuestionExerciseResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Did not found any Question.",
+                    content = @Content
+            )
+    })
     @GetMapping("/random")
     public ResponseEntity<QuestionExerciseResponse> findRandomQuestion() {
         return questionExerciseViewService.findRandomQuestion();
--- backend/src/java/com/reallylastone/quiz/exercise/translation/controller/TranslationExerciseController.java
+++ backend/src/java/com/reallylastone/quiz/exercise/translation/controller/TranslationExerciseController.java
@@ -2,6 +2,11 @@ package com.reallylastone.quiz.exercise.translation.controller;
 
 import com.reallylastone.quiz.exercise.translation.model.TranslationExerciseResponse;
 import com.reallylastone.quiz.exercise.translation.service.TranslationExerciseViewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/exercise/translation")
 @RequiredArgsConstructor
+@Tag(
+        description = "Provides operations to work on Translation Exercise objects",
+        name = "Translation Exercise Controller")
 public class TranslationExerciseController {
     private final TranslationExerciseViewService translationViewService;
 
+    @Operation(
+            summary = "Gets Translation object by its id.", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The specified Translation is present.",
+                    content = @Content(schema = @Schema(implementation = TranslationExerciseResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "The specified Translation was not found.",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<TranslationExerciseResponse> findById(@PathVariable Long id) {
         return translationViewService.findById(id);
