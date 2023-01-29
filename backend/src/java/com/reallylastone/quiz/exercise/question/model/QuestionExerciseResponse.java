d9fda4c6ed96bed810f0b6475496a9df66a0e6be
ReallyLastOne <reallylastone@op.pl>
2023-01-29 08:16:39 +0100

    change response models to records

--- backend/src/java/com/reallylastone/quiz/exercise/question/model/QuestionExerciseResponse.java
+++ backend/src/java/com/reallylastone/quiz/exercise/question/model/QuestionExerciseResponse.java
@@ -1,14 +1,6 @@
 package com.reallylastone.quiz.exercise.question.model;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-public class QuestionExerciseResponse {
-    private String question;
-
-    private String correctAnswer;
-
-    private List<String> wrongAnswers;
+public record QuestionExerciseResponse(String question, String correctAnswer, List<String> wrongAnswers) {
 }
--- backend/src/java/com/reallylastone/quiz/exercise/translation/model/TranslationExerciseResponse.java
+++ backend/src/java/com/reallylastone/quiz/exercise/translation/model/TranslationExerciseResponse.java
@@ -1,13 +1,7 @@
 package com.reallylastone.quiz.exercise.translation.model;
 
-import lombok.Data;
-
 import java.util.Locale;
 import java.util.Map;
 
-@Data
-public class TranslationExerciseResponse {
-    private Map<Locale, String> translationMap;
-
-    private String imagePath;
+public record TranslationExerciseResponse(Map<Locale, String> translationMap, String imagePath) {
 }
