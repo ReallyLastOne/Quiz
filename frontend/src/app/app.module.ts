f0a74bdab325c12d78b4af3f26997f0604501d39
d.janoszka <janoszka.damian@wp.pl>
2023-05-09 14:09:03 +0200

    login_page - simplify css files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,18 @@ import { ErrorComponent } from './error/error.component';
 import { ScoreComponent } from './score/score.component';
 import { ScoreService } from './score/score.service';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExerciseComponent,
+    HomeComponent,
     ErrorComponent,
     ScoreComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule],
   providers: [ScoreService],
