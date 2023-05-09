5bf8112e9a5b3aca1a248a78fdd902b7ca65b0af
d.janoszka <janoszka.damian@wp.pl>
2023-05-09 14:07:10 +0200

    login_page - add a clickable text to switch to registration form

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  login = true;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  registerMeClick() {
+    this.login = false;
   }
-
 }
