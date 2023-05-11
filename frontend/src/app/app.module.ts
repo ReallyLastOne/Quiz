c61bacfa6fce6ead785897b9a5f142edb9ea616a
d.janoszka <janoszka.damian@wp.pl>
2023-05-11 15:28:24 +0200

    frontend_jwt_configuration - add registration form with validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,12 @@ import { RegisterComponent } from './register/register.component';
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
   providers: [ScoreService],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UserAuthenticationService } from '../services/user-authentication.service';
+import { User } from '../model/User.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { error } from 'console';
 
 @Component({
   selector: 'app-register',
@@ -6,13 +10,65 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  private _regForm: FormGroup;
+  private _submitted = false;
+  constructor(private _userAuthenticationService: UserAuthenticationService) {}
 
-  ngOnInit(): void {}
+  get regForm(): FormGroup {
+    return this._regForm;
+  }
+
+  get submitted(): boolean {
+    return this._submitted;
+  }
+
+  set submitted(value: boolean) {
+    this._submitted = value;
+  }
+
+  ngOnInit(): void {
+    this._regForm = new FormGroup({
+      nickname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
 
   @Output() backToLogin = new EventEmitter<boolean>();
 
   backToLoginClick() {
     this.backToLogin.emit(false);
+  }
+
+  onRegister() {
+    this.submitted = true;
+    if (this._regForm.invalid) {
+      return;
+    } else {
+      let user: User;
+      user = new User(
+        this._regForm.get('nickname').value,
+        this._regForm.get('email').value,
+        this._regForm.get('password').value
+      );
+      this._userAuthenticationService.registration(user).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  get registerForm() {
+    return this.regForm.controls;
   }
 }
