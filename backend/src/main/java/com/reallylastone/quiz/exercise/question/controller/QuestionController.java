0697c29d60fbb481891cbee4610841934ea439cd
ReallyLastOne <reallylastone@op.pl>
2023-05-21 12:03:11 +0200

    Implement interface driven controllers
    Add documentation to controller classes
    Make controller classes more readable due to swagger documentation
    Add devtools dependency
    Fix test failures when they were executed all at once

--- backend/src/main/java/com/reallylastone/quiz/exercise/question/controller/QuestionController.java
+++ backend/src/main/java/com/reallylastone/quiz/exercise/question/controller/QuestionController.java
@@ -1,17 +1,11 @@
 package com.reallylastone.quiz.exercise.question.controller;
 
 import com.reallylastone.quiz.exercise.question.service.QuestionViewService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/exercises/questions")
 @RequiredArgsConstructor
-@Tag(
-        description = "Provides operations to work on Question objects",
-        name = "Question Controller")
-public class QuestionController {
+public class QuestionController implements QuestionOperations {
     private final QuestionViewService questionViewService;
 }
--- backend/src/main/java/com/reallylastone/quiz/game/core/translation/controller/TranslationGameController.java
+++ backend/src/main/java/com/reallylastone/quiz/game/core/translation/controller/TranslationGameController.java
@@ -3,8 +3,6 @@ package com.reallylastone.quiz.game.core.translation.controller;
 import com.reallylastone.quiz.game.core.translation.service.TranslationGameViewService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +10,10 @@ import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/game/translation")
-public class TranslationGameController {
+public class TranslationGameController implements TranslationGameOperations {
     private final TranslationGameViewService translationGameViewService;
 
-    @PostMapping
+    @Override
     public Long startGame(@RequestParam Locale sourceLanguage, @RequestParam Locale destinationLanguage, HttpServletRequest request) {
         return translationGameViewService.startGame(sourceLanguage, destinationLanguage, request);
     }
