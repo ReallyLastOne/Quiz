52a3a9b54677db0452f470466bd5ecb12b4ec2b7
d.janoszka <janoszka.damian@wp.pl>
2023-05-31 14:22:14 +0200

    frontend_jwt_configuration - setting jwt id_token in local storage

--- frontend/src/app/services/user-authentication.service.ts
+++ frontend/src/app/services/user-authentication.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { RegistrationRequest } from '../model/registration-request.model';
 import { environment } from '../../environments/environment';
 import { LoginRequest } from '../model/login-request.model';
+import { tap, shareReplay } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +15,44 @@ export class UserAuthenticationService {
   registration(registrationRequest: RegistrationRequest) {
     return this.http.post(
       environment.apiUrl + `/auth/register`,
-      JSON.stringify(registrationRequest)
+      JSON.parse(JSON.stringify(registrationRequest))
     );
   }
   signIn(loginRequest: LoginRequest) {
-    return this.http.post(
-      environment.apiUrl + '/auth/authenticate',
-      loginRequest
-    );
+    return this.http
+      .post(
+        environment.apiUrl + '/auth/authenticate',
+        JSON.parse(JSON.stringify(loginRequest))
+      )
+      .pipe(
+        tap((res) => this.setSession(res)),
+        shareReplay()
+      );
+  }
+
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+    console.log(authResult);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
   }
 }
