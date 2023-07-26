97ef3a4dafa41d14d467d0acc9acb916ec1b3ea7
ReallyLastOne <reallylastone@op.pl>
2023-07-26 19:43:38 +0200

    Get all user phrases endpoint

--- backend/src/main/java/com/reallylastone/quiz/exercise/phrase/controller/PhraseOperations.java
+++ backend/src/main/java/com/reallylastone/quiz/exercise/phrase/controller/PhraseOperations.java
@@ -9,16 +9,26 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Tag(description = "Provides operations to work on Phrase objects", name = "Phrase Controller")
-@RequestMapping(value = "/api/v1/exercises/phrases", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/exercises/phrases", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface PhraseOperations {
 
     @Operation(summary = "Creates or merges if possible a phrase translations. Phrase is created for user or global context")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "422", description = "If content is wrong or if there are translations that cannot be merge into another phrase", content = @Content)})
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<PhraseView> createPhrase(@RequestBody PhraseCreateRequest createRequest);
+
+    @Operation(summary = "Get all phrases for currently authenticated user")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK", content = @Content)})
+    @GetMapping
+    ResponseEntity<List<PhraseView>> getAllPhrases(@RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "100") int size);
 }
--- backend/src/main/java/com/reallylastone/quiz/exercise/phrase/service/PhraseViewService.java
+++ backend/src/main/java/com/reallylastone/quiz/exercise/phrase/service/PhraseViewService.java
@@ -4,8 +4,12 @@ import com.reallylastone.quiz.exercise.phrase.model.PhraseCreateRequest;
 import com.reallylastone.quiz.exercise.phrase.model.PhraseView;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface PhraseViewService {
     ResponseEntity<PhraseView> findById(Long id);
 
     ResponseEntity<PhraseView> createPhrase(PhraseCreateRequest createRequest);
+
+    ResponseEntity<List<PhraseView>> getAllPhrases(int page, int size);
 }
