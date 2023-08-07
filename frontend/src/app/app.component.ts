8743819b155fa7dd5688c4cacad2288f19fc0ca7
Damian <damian1998.10@gmail.com>
2023-08-07 20:37:59 +0200

    Update angular material 13 -> 14

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { UserAuthenticationService } from './services/user-authentication.servic
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
-  constructor(private readonly userAuthenticationService: UserAuthenticationService) { }
+  constructor(
+    private readonly userAuthenticationService: UserAuthenticationService
+  ) {}
 
   get userAuthService() {
     return this.userAuthenticationService;
