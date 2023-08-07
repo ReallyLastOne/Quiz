cbf13586aa5ff59509d1043067bbff68db5e0e82
Damian <damian1998.10@gmail.com>
2023-08-07 20:19:02 +0200

    Fixed angular material and changed navbar

--- frontend/src/app/navbar/navbar.module.ts
+++ frontend/src/app/navbar/navbar.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { NavbarRoutingModule } from './navbar-routing.module';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
-  imports: [CommonModule, NavbarRoutingModule],
+  imports: [
+    CommonModule,
+    NavbarRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+  ],
   declarations: [NavbarComponent],
   exports: [NavbarComponent],
   providers: [],
