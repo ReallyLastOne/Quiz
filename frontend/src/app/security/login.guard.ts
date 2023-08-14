651b27d74b5d91aba19986714b21c17f2facd726
Damian <damian1998.10@gmail.com>
2023-08-14 18:05:03 +0200

    upgraded dependencies and code

--- frontend/src/app/security/login.guard.ts
+++ frontend/src/app/security/login.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable, inject } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserAuthenticationService } from '../services/user-authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
   constructor(
     private readonly _userAuthService: UserAuthenticationService,
     private readonly _router: Router
