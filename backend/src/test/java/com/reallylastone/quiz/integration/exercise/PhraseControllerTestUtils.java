f30c0586f8a614803c5d5da8068bbebd0eb58e93
ReallyLastOne <reallylastone@op.pl>
2023-08-17 20:01:55 +0200

    Tests to batch phrase creation

--- backend/src/test/java/com/reallylastone/quiz/integration/exercise/PhraseControllerTestUtils.java
+++ backend/src/test/java/com/reallylastone/quiz/integration/exercise/PhraseControllerTestUtils.java
@@ -1,18 +1,19 @@
 package com.reallylastone.quiz.integration.exercise;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reallylastone.quiz.exercise.phrase.model.CSVFileParser;
 import com.reallylastone.quiz.exercise.phrase.model.PhraseCreateRequest;
 import com.reallylastone.quiz.integration.EndpointPaths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @Component
 @RequiredArgsConstructor
@@ -37,5 +38,29 @@ public class PhraseControllerTestUtils {
         if (languages != null && languages.length != 0) builder.queryParam("languages", languages);
 
         return mockMvc.perform(builder);
+    }
+
+    public ResultActions createPhrases(CSVFileParser parser, String accessToken, MockMultipartFile file) throws Exception {
+        MockHttpServletRequestBuilder builder = multipart(EndpointPaths.Phrase.BASE)
+                .file("file", file.getBytes())
+                .with(csrf().asHeader())
+                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .header("Authorization", "Bearer " + accessToken);
+
+        if (parser != null) {
+            builder.content(mapper.writeValueAsString(parser));
+        }
+
+        return mockMvc.perform(builder);
+    }
+
+
+    @AllArgsConstructor
+    @Getter
+    public static class BatchPhraseCreateCase {
+        private final String data;
+        private final int correctPhrases;
+        private final int wrongPhrases;
+        private final int resultativePhrasesCount;
     }
 }
