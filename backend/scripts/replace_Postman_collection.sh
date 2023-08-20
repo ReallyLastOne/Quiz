059416cd1f6baff9cf1c14bac80799498dab6f00
ReallyLastOne <reallylastone@op.pl>
2023-08-20 12:15:37 +0200

    Improve script for Postman collection generation - setting csrf environmental variable after every request
    Add Postman environment JSON

--- backend/scripts/replace_Postman_collection.sh
+++ backend/scripts/replace_Postman_collection.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-OPEN_API_DATA=$(curl http://localhost:8080/api/v1/docs-json)
-TEMP_FILE=temporary_open_api_data.json
-echo $TEMP_FILE
-echo $OPEN_API_DATA >> TEMP_FILE
+# set variables
+OPEN_API_DATA_FILE='OPEN_API_DATA_FILE.json'
+OUTPUT_FILE='../tests/postman/Quiz.postman_collection.json'
 
-openapi2postmanv2 -s TEMP_FILE -o ../tests/postman/'Quiz API documentation.postman_collection.json' -p -O folderStrategy=Tags,includeAuthInfoInExample=false
-rm TEMP_FILE
+# fetch Open API data from running application
+curl http://localhost:8080/api/v1/docs-json >> $OPEN_API_DATA_FILE
+
+# convert Open API definition to Postman collection using openapi2postmanv2 npm module
+openapi2postmanv2 -s $OPEN_API_DATA_FILE -o $OUTPUT_FILE -p -O folderStrategy=Tags,includeAuthInfoInExample=false
+
+# add post request procedure - setting csrf token environmental variable
+jq '.event += [{"listen": "test", "script": {"type": "text/javascript", "exec": ["pm.environment.set(\"csrfToken\", pm.cookies.get(\"XSRF-TOKEN\"));"]}}]' $OUTPUT_FILE > temp && mv temp $OUTPUT_FILE
+
+# clean up
+rm $OPEN_API_DATA_FILE
